f2843e3f95779849449b9aa1f47661667dafe51a
bcbell <coleman.britt@gmail.com>
2020-11-13 23:11:11 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # tic-tac-toe
+1. Define the required constants
+
+2. Define the required variables used to track the state of the game
+
+3. Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable, and performant.
+
+4. Upon loading, the app should:
+	4.1- Initialize the state variables
+	4.2- Render those values to the page
+	4.3- Wait for the user to click a square
+
+5. Handle a player clicking a square
+
+6. Handle a player clicking the replay button
+
+                        Design Layout: 
+Color Theme: 
+Colors: Black and gold, Neon-Magenta,Electric Blue, Neon Yellow
+Background-color: Black
+Broad-color/square outline: Gold / Neon Yellow
+Header Text: Orange border with yellow filler (lights) 
+Other Text: White 
+
+Font:
+Heading: Scriptorama Tradeshow JF
+Text and X and O: Franklin Gothic
+
+Button:
+Replay
+Light to Dark 
+
+                        Functionality:
+
+Objective: 
+To win game through clicking on squares that lead to a 3 aligned or adjustant X's or O's
+
+Players: 
+Player One: Human User
+Player Two: Computer 
+
+Constants:
+Board
+
+
--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
+const board =[];
+
+const playerXO ={
+    '1':'magenta',
+    '-1': 'eletric blue',
+    null: 'white',
+}
+
+const winningCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
 
 
@@ -7,16 +25,31 @@
 // Variables might include (board/turn/winner)
 
 /*------------------------ Cached Element References ------------------------*/
-
+const resetBtn= document.getElementById('resetButton')
 // You might choose to put your game status here
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+// resetBtn.addEventListener('click', function(){//reset Button
+//     init()
+// })
 // This is where you should put the event listener
 // for a mouse-click
 
 /*-------------------------------- Functions --------------------------------*/
+init () //initiate 
 
+
+function findAWinner()// if statement using winning combo
+
+function render(){//render
+}
+
+function init(){//initiatization function 
+
+}
+//Board Array
+
+// console.log(createBoard)
 
 // Some functions you might choose to use:
 
