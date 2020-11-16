80c4ef236be74ffa11647052e4c384dfb5cb3e1c
bcbell <coleman.britt@gmail.com>
2020-11-16 02:51:25 -0500

    commit

--- js/app.js
+++ js/app.js
@@ -65,11 +65,23 @@ for (square of squares){
 // lightToDark.addEventListener('click', turnItOff)
 
 /*-------------------------------- Functions --------------------------------*/ 
+const theWinnerIs = (letter) => letter === 'X' ? playerX : playerO
+
+const findAWinner = (letter) => {
+    playGame = false
+    if (letter === 'X'){
+        message.innerHTML= `<span>${theWinnerIs(letter)} has won!</span>`    
+    } else{
+        message.innerHTML= `<span>${theWinnerIs(letter)} has won!</span>`     
+    }
+}
+
 
 const statusUpdate = () => {
     const squareTL = squares[0].classList[2]
     const squareTC = squares[1].classList[2]
     const squareTR = squares[2].classList[2]
+    
     const squareML = squares[3].classList[2]
     const squareMC = squares[4].classList[2]
     const squareMR = squares[5].classList[2]
@@ -77,13 +89,27 @@ const statusUpdate = () => {
     const squareBC = squares[7].classList[2]
     const squareBR = squares[8].classList[2]
 
+
 //Checking for a winner 
-    if(squareTL && squareTL === squareTC && squareTL === squareTR){
+        if (squareTL && squareTL ===squareTC && squareTL ===squareTR){
+    } else if(squareML && squareML === squareMC && squareML=== squareMR){ 
+         findAWinner(squareML);
+    } else if (squareBL && squareBL=== squareBC && squareBL=== squareBR){
+         findAWinner(squareBL);
+    } else if (squareTL && squareTL === squareML  && squareTL=== squareBL){
+         findAWinner(squareTL);
+    } else if (squareTC && squareTC === squareMC && squareTC === squareBC){
+          findAWinner(squareTC);
+    } else  if(squareTR && squareTR === squareMR && squareTR=== squareBR){
+         findAWinner(squareTR);
+    }else if (squareTL && squareTL === squareMC && squareTL === squareBR){
+        findAWinner(squareTL); 
+    } else if (squareTR && squareTR === squareMC && squareTR=== squareBL){
+        findAWinner(squareTR);
+    } else if(squareTL && squareTC && squareTR && squareML && squareMC && squareMR && squareBL && squareBC && squareBR){
         playGame =false
-        winner = squareTL
-        message.innerHTML= `${squareTL} has won!`
-    }
+        message.innerHTML=  `<span>This game ends in a tie! Try again!</span>`
+}  
+
 }
-
-
 
